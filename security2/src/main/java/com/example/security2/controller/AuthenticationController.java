cac6f13224529ef824073055966dd6a74ac4d23b
R. Arnanda Adi Wijanarko <134377157+ArnandaAdi@users.noreply.github.com>
2024-04-01 10:00:11 +0700

    Add files via upload

---
+++ security2/src/main/java/com/example/security2/controller/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.example.security2.controller;
+
+import com.example.security2.utils.AuthenticationResponse;
+import com.example.security2.service.AuthenticationService;
+import com.example.security2.utils.AuthenticationRequest;
+import com.example.security2.utils.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private AuthenticationService service;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return  ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return  ResponseEntity.ok(service.authenticate(request));
+    }
+}
